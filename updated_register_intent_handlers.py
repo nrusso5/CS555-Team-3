da392136f2e255f1da0f672ba1fc5351f197e125
nrusso5 <165161758+nrusso5@users.noreply.github.com>
2024-10-17 19:39:56 -0400

    Adding VitalLinkHelpIntentHandler python intent handler registration

---
+++ updated_register_intent_handlers.py
@@ -0,0 +1,7 @@
+# Register intent handlers
+sb.add_request_handler(GetNewFactHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(VitalLinkHelpIntentHandler()) 
