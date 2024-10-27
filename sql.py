d9f1b5cf7677c2fea79cbdd9e7ddba156e4965af
ffusername <kylemcmurray41@gmail.com>
2024-10-27 18:53:21 -0400

    encryption and logging added

--- sql.py
+++ sql.py
@@ -1,60 +1,86 @@
 import mysql.connector
+from cryptography.fernet import Fernet
+
 class mySQLdb:
-    def __init__(self, host, user, password, database):
+    def __init__(self, host, user, password, database, key=None):
         self.conn = mysql.connector.connect(
-            host='--',
-            user='--',
-            password='--',
-            database='--'
+            host='localhost',
+            user='root',
+            password='PAssword21!',
+            database='test_db'
         )
+        if not key:
+            self.key = Fernet.generate_key()
+        else:
+            self.key = key
+        self.cipher = Fernet(self.key)
         self.cursor = self.conn.cursor()
 
+    def encrypt_data(self, data):
+        return self.cipher.encrypt(data.encode())
+
+    def decrypt_data(self, data: bytes):
+        return self.cipher.decrypt(data).decode()
+
     def insert_contact_info(self, full_name, phone_number):
+        """
+        Send contact information into mysql database
+        """
+        full_name = self.encrypt_data(full_name)
+        phone_number = self.encrypt_data(full_name)
+
         sql = "INSERT INTO contactinformation (full_name, phone_number) VALUES (%s, %s)"
         self.cursor.execute(sql, (full_name, phone_number))
         self.conn.commit()
         print(f'User {full_name}, with phone number: {phone_number} added')
 
     def insert_health_information(self, id, comments):
+        """
+        Send health information into mysql database
+        """
+        comments = self.encrypt_data(comments)
         sql = "INSERT INTO healthinformation (id, comments) VALUES (%s, %s)"
         self.cursor.execute(sql, (id, comments))
         self.conn.commit()
         print(f'User {id} added')
 
     def fetch_contact_by_id(self, id):
+        """
+        Fetch contact information by userID
+        """
         sql = "SELECT * FROM contactinformation WHERE ID = %s"
         self.cursor.execute(sql, (id,))
         results = self.cursor.fetchall()
-        return results
-        # print(f"user with {id}'s information: {results}")
-        # print(f'Types: id:{type(id)}, results: {type(results[0][0])} {type(results[0][1])} {type(results[0][2])}')
+        results = self.decrypt_data(results[0][1])
+        results2 = self.decrypt_data(results[0][2])
+        return results, results2
 
     def fetch_health_information_by_id(self, id):
+        """
+        Fetch health information by userID 
+        """
         sql = "SELECT * FROM healthinformation WHERE ID = %s"
         self.cursor.execute(sql, (id,))
         results = self.cursor.fetchall()
+        results = self.decrypt_data(results[0][0])
+        results2 = self.decrypt_data(results[0][1])
         return results
 
-    def fetch_table(self, table):
-        self.cursor.execute(f'SELECT * FROM {table}')
-        results = self.cursor.fetchall()
-        for row in results:
-            print(row)
+    def log_changes(self, user, database, action, time):
+        """
+        Log all changes made to databases healthinformation and contactinformation
+        """
+        user = self.encrypt_data(user)
+        database = self.encrypt_data(database)
+        action = self.encrypt_data(action)
+        time = self.encrypt_data(time)
+
+        sql = "INSERT INTO audit_log (user, database, action, time) VALUES (%s, %s, %s, %s)"
+        self.cursor.execute(sql, (user, database, action, time))
+        self.conn.commit()
+        print("Changelog saved")
+
+
 
     if __name__ == '__main__':
-        insert_contact_info("John doe", "9999999999")
-
-        insert_health_information("1", "comments about the health of this person with user id 1")
-        fetch_contact_by_id(1)
-        fetch_table("contactinformation")
-        fetch_table("healthinformation")
-
-        # insert_user('Alice', 'alice@example.com')
-        # insert_user('Bob', 'bob@example.com')
-
-        # print("Current users in the database:")
-        # fetch_users()
-
-        # Close the cursor and connection
-        # self.cursor.close()
-        # self.conn.close()
+        quit()
