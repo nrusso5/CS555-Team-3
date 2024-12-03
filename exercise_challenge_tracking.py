0af30e91811d89560f5e79c0e1258cee17fe741d
AryaSriva <87484641+AryaSriva@users.noreply.github.com>
2024-12-03 17:39:59 -0500

    added comments

--- exercise_challenge_tracking.py
+++ exercise_challenge_tracking.py
@@ -1,17 +1,22 @@
 class ExerciseTracker:
     def __init__(self, exercise: str, goal: int) -> None:
+        # initialize the tracker
         if goal <= 0:
             raise ValueError("goal has to be > 0")
+        if exercise == "":
+            raise ValueError("exercise is empty")
         self.exercise = exercise
         self.goal = goal
         self.currProgress = 0
 
     def progress(self, progression: int) -> None:
+        # update the user's progress
         if progression <= 0:
             raise ValueError("goal has to be > 0")
         self.currProgress += progression
 
     def goalReached(self) -> None:
+        # inform the user that they have reached their goal and
         if self.currProgress == self.goal:
             self.resetProgress()
             return True
@@ -19,9 +24,11 @@ class ExerciseTracker:
             return False
 
     def setNewGoal(self, goal: int) -> None:
+        # set a new goal
         if goal <= 0:
             raise ValueError("goal has to be > 0")
         self.goal = goal
 
     def resetProgress(self) -> None:
+        # reset progress(helper for goal reached)
         self.currProgress = 0
